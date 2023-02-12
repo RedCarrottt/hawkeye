b0e72972ed370646aff26a88a4f6bec82f81145e
Gyeonghwan Hong <redcarrottt@gmail.com>
2023-02-12 22:47:53 +0900

    hawkeye: refactor code
    Signed-off-by: Gyeonghwan Hong <redcarrottt@gmail.com>

--- hawkeye.py
+++ hawkeye.py
@@ -11,17 +11,68 @@ class DiagramDrawer:
     def __init__(self, filename):
         self.filename = filename
 
-    def __layoutRecursively(self, node, state, parentDiag):
-        diags = []
-        nodeDiag = None
-
+    def __addRectangle(self, node, layoutState, parentDiag):
         MIN_WIDTH = 150
         MIN_HEIGHT = 30
         INDENT_WIDTH = 10
         ROW_HEIGHT = MIN_HEIGHT + 15
         MARGIN = 10
+
+        left = layoutState['left'] + INDENT_WIDTH * node.indent
+        bottom = layoutState['bottom'] - ROW_HEIGHT
+        width = MIN_WIDTH
+        height = MIN_HEIGHT
+        layoutState['bottom'] = bottom
+        
+        nodeDiag = {
+            'type': 'rectangle',
+            'pos': [left, bottom, width, height],
+            'margin': [MARGIN, MARGIN],
+            'textSize': 16,
+            'labelText': node.labelText
+        }
+        if isinstance(node, IterationNode) or isinstance(node, BranchNode):
+            nodeDiag['stroke_width'] = 4
+        else:
+            nodeDiag['stroke_width'] = 2
+        return nodeDiag
+
+    def __addLineToRectangle(self, nodeDiag, layoutState, parentDiag):
+        # To this rectangle (child)
         PARENT_LINE_START_X_RATIO = 0.1
         PARENT_LINE_END_Y_RATIO = 0.5
+
+        left = nodeDiag['pos'][0]
+        bottom = nodeDiag['pos'][1]
+        width = nodeDiag['pos'][2]
+        height = nodeDiag['pos'][3]
+        child_line_end_x = left
+        child_line_end_y = height * PARENT_LINE_END_Y_RATIO + bottom
+        
+        parentDiagType = parentDiag['type']
+        if parentDiagType == 'rectangle':
+            # From a rectangle (parent)
+            childLeft = parentDiag['pos'][0]
+            childBottom = parentDiag['pos'][1]
+            childWidth = parentDiag['pos'][2]
+            childHeight = parentDiag['pos'][3]
+
+            child_line_start_x = childWidth * PARENT_LINE_START_X_RATIO + childLeft
+            child_line_start_y = childBottom
+
+            lineDiag = {
+                'type': 'line',
+                'path': [child_line_start_x, child_line_start_y,
+                         child_line_start_x, child_line_end_y,
+                         child_line_end_x, child_line_end_y],
+                'scale': 6
+            }
+            return lineDiag
+        return None
+
+    def __layoutRecursively(self, node, layoutState, parentDiag):
+        diags = []
+        nodeDiag = None
 
         nodeDiagType = ''
         if not isinstance(node, Sketch) and not isinstance(node, ForkNode):
@@ -29,67 +80,29 @@ class DiagramDrawer:
 
         # Add diagram for the node
         if nodeDiagType == 'rectangle':
-            left = state['left'] + INDENT_WIDTH * node.indent
-            bottom = state['bottom'] - ROW_HEIGHT
-            width = MIN_WIDTH
-            height = MIN_HEIGHT
-            state['bottom'] = bottom
-            
-            nodeDiag = {
-                'type': 'rectangle',
-                'pos': [left, bottom, width, height],
-                'margin': [MARGIN, MARGIN],
-                'textSize': 16,
-                'labelText': node.labelText
-            }
-            if isinstance(node, IterationNode) or isinstance(node, BranchNode):
-                nodeDiag['stroke_width'] = 4
-            else:
-                nodeDiag['stroke_width'] = 2
+            nodeDiag = self.__addRectangle(node, layoutState, parentDiag)
             diags.append(nodeDiag)
 
         # Add lines from the parent node to this node
         if parentDiag and nodeDiag:
+            lineDiag = None
             if nodeDiagType == 'rectangle':
-                left = nodeDiag['pos'][0]
-                bottom = nodeDiag['pos'][1]
-                width = nodeDiag['pos'][2]
-                height = nodeDiag['pos'][3]
-                child_line_end_x = left
-                child_line_end_y = height * PARENT_LINE_END_Y_RATIO + bottom
-                
-                parentDiagType = parentDiag['type']
-                if parentDiagType == 'rectangle':
-                    childLeft = parentDiag['pos'][0]
-                    childBottom = parentDiag['pos'][1]
-                    childWidth = parentDiag['pos'][2]
-                    childHeight = parentDiag['pos'][3]
-    
-                    child_line_start_x = childWidth * PARENT_LINE_START_X_RATIO + childLeft
-                    child_line_start_y = childBottom
-    
-                    lineDiag = {
-                        'type': 'line',
-                        'path': [child_line_start_x, child_line_start_y,
-                                 child_line_start_x, child_line_end_y,
-                                 child_line_end_x, child_line_end_y],
-                        'scale': 6
-                    }
-                    diags.append(lineDiag)
+                lineDiag = self.__addLineToRectangle(nodeDiag, layoutState, parentDiag)
+            diags.append(lineDiag)
 
         if node.children:
             parentDiag = nodeDiag if nodeDiag else parentDiag
             for childNode in node.children:
-                childDiags = self.__layoutRecursively(childNode, state, parentDiag)
+                childDiags = self.__layoutRecursively(childNode, layoutState, parentDiag)
                 diags += childDiags
         return diags
 
     def layout(self, node):
         WIDTH = 1024
         HEIGHT = 768
-        state = {'width': WIDTH, 'height': HEIGHT, 'left': 10, 'bottom': HEIGHT}
+        layoutState = {'width': WIDTH, 'height': HEIGHT, 'left': 10, 'bottom': HEIGHT}
 
-        diags = self.__layoutRecursively(node, state, None)
+        diags = self.__layoutRecursively(node, layoutState, None)
         layout = {'width': WIDTH, 'height': HEIGHT, 'diags': diags}
         return layout
 
